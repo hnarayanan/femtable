8d264379a5f4a9c6f1175cc80679490b270453c5
Harish Narayanan <hnarayanan@gmail.com>
2014-10-05 16:59:56 +0100

    Make title fill out the space it has

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+jQuery('#responsive-title').fitText(1.70);
+
+MathJax.Hub.Config({
+  messageStyle: 'none',
+  TeX: {
+    Macros: {
+      P: ['\\mathcal{P}_{#1}\\Lambda^{#2}(\\Delta_{#3})', 3],
+      Pm: ['\\mathcal{P}^-_{#1}\\Lambda^{#2}(\\Delta_{#3})', 3],
+      Qm: ['\\mathcal{Q}^-_{#1}\\Lambda^{#2}(\\square_{#3})', 3],
+      S: ['\\mathcal{S}_{#1}\\Lambda^{#2}(\\square_{#3})', 3],
+      Pq: ['\\mathcal{P}_{#1}\\Lambda^{#2}(\\square_{#3})', 3],
+      dx: ['\\mathrm{\\mathrm{d}x^{#1}}', 1],
+      dof: ['#1\\times\\underbrace{\\P{#2}{#3}{#4}}_{#5}', 5],
+      dofm: ['#1\\times\\underbrace{\\Pm{#2}{#3}{#4}}_{#5}', 5],
+      dofq: ['#1\\times\\underbrace{\\Qm{#2}{#3}{#4}}_{#5}', 5],
+      dofs: ['#1\\times\\underbrace{\\Pq{#2}{#3}{#4}}_{#5}', 5],
+      pl: '\\,+\\,',
+      div: '\\operatorname{div}',
+      curl: '\\operatorname{curl}',
+    }
+  }
+});
