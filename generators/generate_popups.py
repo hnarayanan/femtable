3c02871dd58770ec28ffdff0fa1e91df7ea24562
Harish Narayanan <hnarayanan@gmail.com>
2014-10-12 20:30:36 +0100

    Make popup generator aware of equation lengths and try to scale them down to fit

--- generators/generate_popups.py
+++ generators/generate_popups.py
@@ -6,8 +6,21 @@ from element_families.P import P
 from element_families.Qm import Qm
 from element_families.S import S
 
+
+def equation_size(value):
+    terms = value.count("\dof")
+    if terms == 1:
+        return "normal"
+    elif terms == 2:
+        return "small"
+    elif terms == 3:
+        return "tiny"
+    elif terms == 4:
+        return "really-tiny"
+
 loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates"))
 templates = jinja2.Environment(loader=loader)
+templates.filters['equation_size'] = equation_size
 popup_template = templates.get_template("popups.html")
 
 elements_list = {"elements": Pm.items() + P.items() + Qm.items() + S.items()}
