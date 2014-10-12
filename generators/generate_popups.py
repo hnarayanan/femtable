e615f580a2e4cc357a93591e540457714bc489ea
Harish Narayanan <hnarayanan@gmail.com>
2014-10-12 11:30:49 +0100

    Update the popup generator script to use the new elements

--- generators/generate_popups.py
+++ generators/generate_popups.py
@@ -1,13 +1,16 @@
 import os
 import jinja2
 
-from element_families import Pm
+from element_families.Pm import Pm
+from element_families.P import P
+from element_families.Qm import Qm
+from element_families.S import S
 
 loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates"))
 templates = jinja2.Environment(loader=loader)
 popup_template = templates.get_template("popups.html")
 
-elements_list = {"elements": Pm.items()}
+elements_list = {"elements": Pm.items() + P.items() + Qm.items() + S.items()}
 
 generated = popup_template.render(elements_list)
 print generated
