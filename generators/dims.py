53cf9dcc0c9213a0840552a8df84f06ff4898c07
Harish Narayanan <hnarayanan@gmail.com>
2014-10-13 23:27:36 +0100

    Expand dims.py to start writing out an html table

--- generators/dims.py
+++ generators/dims.py
@@ -26,64 +26,71 @@ print "Space P_r^- L^k"
 print "==============="
 print
 for n in range(1, nmax + 1):
-    print "n = %d" % n
-    print "=====\n"
-    for k in range(n + 1):
-        print "k = %d:" % k,
+    for i, k in enumerate(range(n + 1)):
+        if n % 2 == 0:
+            print "<tr>"
+        else:
+            print '<tr class="pure-table-odd">'
+        if i == 0:
+            print "<td>n = %d</td>" % n
+        else:
+            print "<td></td>"
+        print "<td>k = %d</td>" % k
         for r in range(1, qmax + 1):
             dofs = []
             dim = nCk(r + n, r + k)*nCk(r + k - 1, k)
-            print dim,
-        print
+            print "<td>%d</td>" % dim
+        print "</tr>"
     print
 
-# P_r L^k
-print "Space P_r L^k"
-print "============="
-print
-for n in range(1, nmax + 1):
-    print "n = %d" % n
-    print "=====\n"
-    for k in range(n + 1):
-        print "k = %d:" % k,
-        for r in range(1, qmax + 1):
-            dofs = []
-            dim = nCk(r + n, r + k)*nCk(r + k, k)
-            print dim,
-        print
-    print
 
-# Q_r^- L^k
-print "Space Q_r^- L^k"
-print "==============="
-print
-for n in range(1, nmax + 1):
-    print "n = %d" % n
-    print "=====\n"
-    for k in range(n + 1):
-        print "k = %d:" % k,
-        for r in range(1, qmax + 1):
-            dofs = []
-            latex = []
-            dim = nCk(n, k)*(r**k)*((r + 1)**(n - k))
-            print dim,
-        print
-    print
+# # P_r L^k
+# print "Space P_r L^k"
+# print "============="
+# print
+# for n in range(1, nmax + 1):
+#     print "n = %d" % n
+#     print "=====\n"
+#     for k in range(n + 1):
+#         print "k = %d:" % k,
+#         for r in range(1, qmax + 1):
+#             dofs = []
+#             dim = nCk(r + n, r + k)*nCk(r + k, k)
+#             print dim,
+#         print
+#     print
 
-# S_r L^k
-print "Space S_r L^k"
-print "============="
-print
-for n in range(1, nmax + 1):
-    print "n = %d" % n
-    print "=====\n"
-    for k in range(n + 1):
-        print "k = %d:" % k,
-        for r in range(1, qmax + 1):
-            dofs = []
-            latex = []
-            dim = sum([(2**(n - dd))*nCk(n, dd)*nCk(r - dd + 2*k, dd)*nCk(dd, k) \
-                           for dd in range(k, min(n, r/2 + k) + 1)])
-            print dim,
-        print
-    print
+# # Q_r^- L^k
+# print "Space Q_r^- L^k"
+# print "==============="
+# print
+# for n in range(1, nmax + 1):
+#     print "n = %d" % n
+#     print "=====\n"
+#     for k in range(n + 1):
+#         print "k = %d:" % k,
+#         for r in range(1, qmax + 1):
+#             dofs = []
+#             latex = []
+#             dim = nCk(n, k)*(r**k)*((r + 1)**(n - k))
+#             print dim,
+#         print
+#     print
+
+# # S_r L^k
+# print "Space S_r L^k"
+# print "============="
+# print
+# for n in range(1, nmax + 1):
+#     print "n = %d" % n
+#     print "=====\n"
+#     for k in range(n + 1):
+#         print "k = %d:" % k,
+#         for r in range(1, qmax + 1):
+#             dofs = []
+#             latex = []
+#             dim = sum([(2**(n - dd))*nCk(n, dd)*nCk(r - dd + 2*k, dd)*nCk(dd, k) \
+#                            for dd in range(k, min(n, r/2 + k) + 1)])
+#             print dim,
+#         print
+#     print
