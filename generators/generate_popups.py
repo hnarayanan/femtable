6bfdfe262b90db9047c63f84eb1b7daf562eb0ec
Harish Narayanan <hnarayanan@gmail.com>
2014-10-17 22:15:00 +0100

    Clean up generators and css to reduce size of long code snippets

--- generators/generate_popups.py
+++ generators/generate_popups.py
@@ -18,9 +18,17 @@ def equation_size(value):
     elif terms == 4:
         return "really-tiny"
 
+def is_code_breaking(value):
+    if 'quadrilateral' in value and ('BDMC' in value or 'RTC' in value or 'DQ' in value or 'Q' in value):
+        return True
+    else:
+        return False
+
+
 loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates"))
 templates = jinja2.Environment(loader=loader)
 templates.filters['equation_size'] = equation_size
+templates.filters['is_code_breaking'] = is_code_breaking
 popup_template = templates.get_template("popups.html")
 
 elements_list = {"elements": Pm.items() + P.items() + Qm.items() + S.items()}
